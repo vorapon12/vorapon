4650a94814fb9a30a3e865dba772b2b31d2a76b8
vorapon12 <112742475+vorapon12@users.noreply.github.com>
2025-05-14 13:27:12 +0700

    mos1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   console.log("mos2");
 
+  console.log("mos1");
+
   return <div className={styles.page}>Hello world!</div>;
 }
